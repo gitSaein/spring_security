a228bfe34c6d1132cf4fa2bdff8c6856e2178bc9
saein <saein@saein-PC>
2019-02-09 20:03:13 +0900

    spring security start

---
+++ spring_security/src/main/java/com/example/saein/SaeinApplication.java
@@ -0,0 +1,14 @@
+package com.example.saein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaeinApplication.class, args);
+	}
+
+}
+
